8e7f2dfb3db627e9ef6dfb0567558664f2c9c0b0
21pm18 <157006573+21pm18@users.noreply.github.com>
2024-11-08 09:08:25 +0100

    bouton rouge
    mis le bouton en rouge

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,11 @@
 // TODO
 const aMettreEnRouge= document.querySelector ("#a-meter-en-rouge");
 aMettreEnRouge.style.color = "red";
+
+const aMettreEnRouge = document.querySelector("#a-mettre-en-rouge");
+aMettreEnRouge.style.color = "red";
+
+const enRougeSuiteAClick = document.querySelector("#en-rouge-suite-a-click");
+enRougeSuiteAClick.addEventListener("click", (evt) => {
+    enRougeSuiteAClick.style.color = "red";
+});
